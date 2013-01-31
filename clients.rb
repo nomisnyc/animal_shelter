39c2df4657110fd4db1aa5a58a1ef19b25af18e9
Simon Wong <nomisnyc85@gmail.com>
2013-01-31 18:22:39 -0500

    add client funct

--- clients.rb
+++ clients.rb
@@ -11,4 +11,24 @@ class Client
     @email = email
     @dob = dob
   end
+
+
+
+def new_client
+  print "Name: "
+  name = gets.chomp.downcase
+  print "Age: "
+  age = gets.chomp.to_i
+  print "Address: "
+  address = gets.chomp.downcase.to_s
+  print "Phone: "
+  phone = gets.chomp.to_i
+  print "Email: "
+  email = gets.chomp.to_s
+  print "Date of Birth(mm/dd/yy): "
+  dob = gets.chomp.to_s
+  Client.new(name, age, address, phone, email, dob)
 end
+
+end
+
