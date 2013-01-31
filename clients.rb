45ec505e8fcb7e736593732704a7aa4faeddec38
Simon Wong <nomisnyc85@gmail.com>
2013-01-31 18:03:17 -0500

    add client

---
+++ clients.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Client
+  attr_accessor :name, :age, :address, :phone, :email :dob
+
+  def initialize(name, age, address, phone, email, dob)
+    @name = name
+    @age = age
+    @address = address
+    @phone = phone
+    @email = email
+    @dob = dob
+  end
+end
